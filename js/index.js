7afd59b5935802269fa852e1eb3fe4804567e055
Ashley <54685244+ashleyalmay@users.noreply.github.com>
2020-04-07 18:36:38 -0400

    start

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+.textContent = addEvenListener(
+  "dblclick",
+  window.scrollBy(0, 0)
+);
+
+
+const get = selector => {
+    document.querySelector(".content-pic .btn");
+};
+get.addEvenListener('click',)
