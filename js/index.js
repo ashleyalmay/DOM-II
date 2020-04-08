cdf1efc7a991fe76ea8242899e083ff4a1d913fa
Ashley <54685244+ashleyalmay@users.noreply.github.com>
2020-04-07 21:05:04 -0400

    a few done

--- js/index.js
+++ js/index.js
@@ -1,12 +1,30 @@
-// Your code goes here
-
-.textContent = addEvenListener(
-  "dblclick",
-  window.scrollBy(0, 0)
-);
-
+// // Your code goes here
 
 const get = selector => {
-    document.querySelector(".content-pic .btn");
+  return document.querySelector(selector);
 };
-get.addEvenListener('click',)
+
+const button = get(".content-pick .btn");
+
+button.addEventListener("mouseover", () => {
+  button.style.color = "#A0522D";
+});
+
+const getall = selector => {
+  return document.querySelectorAll(".content-pick .btn");
+};
+
+const buttons = getall(".content-pick .btn");
+
+buttons[1].addEventListener("click", () => {
+  alert("funbus");
+});
+buttons[2].addEventListener("dblclick", () => {
+  buttons[2].style.color = "#A0522D";
+});
+
+// for (let i = 0; i < buttons.length; i++) {
+//   buttons[i].addEventListener("click", () => {
+//     alert("test");
+//   });
+// }
